62e5d552c52ba2454dffc204a0038300ed5ecaf3
jkcrts <jk.crtes@gmail>
2021-11-27 03:01:38 +0800

    Finished associate back end functions

--- Finals/manage-items.php
+++ Finals/manage-items.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include ("dbconnect.php");
-$query = "SELECT * from inventory where category !='ECOMMERCE'";
+$query = "SELECT * from inventory where category ='STORE'";
 $result = mysqli_query($conn, $query);
 
 ?>
@@ -33,95 +33,172 @@ $result = mysqli_query($conn, $query);
         </a>
 
         <div class="login-as-text-container">
-            <h2>Logged in as, <a href="assoc-menu.php">Associate</a></h2>
+            <h2>Logged in as, <a href="assoc-menu.php"><?php echo $_SESSION['username'];?></a></h2>
         </div>
     </header>
 
     <section class="grid-page-2">
         <div class="left"><a href="assoc-menu.php"><b>←</b></a></div>
         <div class="center">
-            <form action="#">
+            <form action="#" method="POST" enctype="multipart/form-data">
                 <div class="manage-items-form-container">
                     <div class="item-row1">
-                        <input type="text" name="item-name" id="item-name" placeholder="Enter Item Name">
+                        <input type="text" name="item_name" id="item-name" placeholder="Enter Item Name">
                     </div>
                     <div class="item-row2">
                         <label for="item-available">Available:</label>
-                        <input type="text" name="item-available" id="item-available">
+                        <input type="text" name="item_stock" id="item-available">
                         <label for="item-group">Image:</label>
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <label for="item-price">Price:</label>
-                        <input type="text" name="item-price" id="item-price">
+                        <input type="text" name="item_price" id="item-price">
                     </div>
                     <div class="item-row3">
-                        <textarea name="item-info" id="additional-info" cols="30" rows="10" placeholder="Enter Additional Info"></textarea>
+                        <textarea name="item_info" id="additional-info" cols="30" rows="10" placeholder="Enter Additional Info"></textarea>
                     </div>
                     <div class="item-submit">
-                        <input type="submit" value="+">
+                        <input type="submit" name = 'submit' value="+">
                     </div>
                 </div>
+
+                <?php
+
+                    if (isset($_POST['submit'])){
+
+                        $item_name = $_POST['item_name'];
+                        $item_stock = $_POST['item_stock'];
+                        $item_price = $_POST['item_price'];
+                        $item_info = $_POST['item_info'];
+                        $item_seller = $_SESSION['business_id'];
+
+                        
+
+                        $insert = "INSERT INTO inventory (business_id, item_name, item_description, category, price, stock) VALUES ($item_seller, '$item_name', '$item_info', 'STORE', $item_price, $item_stock)";
+                        $query = mysqli_query($conn, $insert);
+
+                        $upload_count = count($_FILES['item_group']['name']);
+                        $dir = "Items";
+
+                        $command = "SELECT * FROM inventory WHERE item_name = '$item_name' AND business_id = '$item_seller'";
+                        $read = mysqli_query($conn, $command);
+                        $qValue = mysqli_fetch_array($read);
+                        $business_id = $qValue['product_id'];
+                        
+                        for($i = 0; $i < $upload_count; $i++){
+                            $filename = $_FILES['item_group']['name'][$i];
+                            $upload = move_uploaded_file($_FILES['item_group']['tmp_name'][$i], $dir. "/". $_FILES['item_group']['name'][$i]);
+                            $insert = "INSERT INTO product_photos (product_id, filename) values ($business_id, '$filename')";
+                            $query = mysqli_query($conn, $insert);
+                        }
+
+                        header("Location: manage-items.php");
+
+                    }
+                
+                
+                ?>
+
             </form>
             <!-- Displaying Items -->
-            <div class="display-container">
+            <?php 
+
+                if (mysqli_num_rows($result) == 0){
+                    
+                } else {
+
+                     while ($qValue = mysqli_fetch_assoc($result)){
+                    
+            ?>
+
+                <div class="display-container">
 
 
-                 <div class="display-infos" id="business-infos">
-                    <div class="display-label">
-                        <h3>Item_ID</h3>
-                    </div>
-                    <div class="display-value">
-                        100000001
-                    </div>
-                </div>  
+                    <div class="display-infos" id="business-infos">
+                        <div class="display-label">
+                            <h3>Item ID</h3>
+                        </div>
+                        <div class="display-value">
+                            <?php echo $qValue['product_id'];?>
+                        </div>
+                    </div>  
 
 
-                <div class="display-infos" id="business-infos">
-                    <div class="display-label">
-                        <h3>Item Name</h3>
-                    </div>
-                    <div class="display-value">
-                        Sample Item 1
-                    </div>
-                </div>  
+                    <div class="display-infos" id="business-infos">
+                        <div class="display-label">
+                            <h3>Item Name</h3>
+                        </div>
+                        <div class="display-value">
+                            <?php echo $qValue['item_name'];?>
+                        </div>
+                    </div>  
 
-                <div class="display-infos" id="business-infos">
-                    <div class="display-label">
-                        <h3>Stock</h3>
+                    <div class="display-infos" id="business-infos">
+                        <div class="display-label">
+                            <h3>Stock</h3>
+                        </div>
+                        <div class="display-value">
+                            <?php echo $qValue['stock'];?>
+                        </div>
                     </div>
-                    <div class="display-value">
-                        Yes
-                    </div>
+                    
+
+                    <div class="display-infos" id="business-infos">
+                        <div class="display-label">
+                            <h3>Price</h3>
+                        </div>
+                        <div class="display-value" id="password-wrap">
+                            <?php echo $qValue['price'];?>
+                        </div>
+                    </div> 
+
+                    <div class="display-infos" id="info-scroll">
+                        <div class="display-label">
+                            <h3>Information</h3>
+                        </div>
+                        <div class="display-value">
+                            <?php echo $qValue['item_description'];?>
+                        </div>
+                    </div> 
                 </div>
-                
+            <?php
+                    }
+                }
+            ?>
 
-                <div class="display-infos" id="business-infos">
-                    <div class="display-label">
-                        <h3>Price</h3>
-                    </div>
-                    <div class="display-value" id="password-wrap">
-                        99.50
-                    </div>
-                </div> 
-
-                <div class="display-infos" id="info-scroll">
-                    <div class="display-label">
-                        <h3>Information</h3>
-                    </div>
-                    <div class="display-value">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsum omnis sint sit voluptatem magni vitae repellendus quam cumque maxime.
-                    </div>
-                </div> 
-            </div>
             <form action="manage-items.php" method="post" id="delete-form">
                     <div class="delete-form-container">
-                        <input type="text" name="business" id="delete-business" placeholder="Enter Account Name">
+                        <input type="text" name="product_id" id="delete-business" placeholder="Enter Product ID">
                         <input type="submit" name="Delete" value="Delete" id="Delete">
                     </div> 
                     
             </form>
+
+
+            <?php 
+                    
+                    if(isset($_POST['Delete'])){
+                        $del_name = $_POST['product_id'];
+
+                        $query = "DELETE FROM product_photos WHERE product_id = $del_name";
+                        mysqli_query($conn, $query);
+                        
+                        $query = "DELETE FROM inventory WHERE product_id = $del_name";
+
+                        if (!mysqli_query($conn, $query)){
+                            echo("Error description: " . mysqli_error($conn));
+                            echo "<h3 style = 'color:red; text-align:center'>No Existing Account Name</h3>";
+                        } else {
+                            echo "<h3> The Change will apply after the refresh</h3>";
+                        }
+
+                    }
+                
+                
+            ?>
+
         </div>
         <div class="right"></div>
     </section>
--- Finals/manage-online-store.php
+++ Finals/manage-online-store.php
@@ -41,38 +41,69 @@ $result = mysqli_query($conn, $query);
     <section class="grid-page-2">
         <div class="left"><a href="assoc-menu.php"><b>←</b></a></div>
         <div class="center">
-            <form action="#">
+            <form action="" method = "POST" enctype="multipart/form-data">
                 <div class="manage-items-form-container">
                     <div class="item-row1">
-                        <input type="text" name="item-name" id="item-name" placeholder="Enter Item Name">
+                        <input type="text" name="item_name" id="item-name" placeholder="Enter Item Name">
                     </div>
                     <div class="item-row2">
                         <label for="item-stock">Stock:</label>
-                        <input type="text" name="item-stock" id="item-stock">
+                        <input type="text" name="item_stock" id="item-stock">
                         <label for="item-group">Photos:</label>
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
+                        <input type="file" name="item_group[]" id="item-group" placeholder="Sample Group 1">
                         <label for="item-price">Price:</label>
-                        <input type="text" name="item-price" id="item-price">
+                        <input type="text" name="item_price" id="item-price">
                     </div>
                     <div class="item-row3">
-                        <textarea name="item-info" id="additional-info" cols="30" rows="10" placeholder="Enter Additional Info"></textarea>
+                        <textarea name="item_info" id="additional-info" cols="30" rows="10" placeholder="Enter Additional Info"></textarea>
                     </div>
                     <div class="item-submit">
-                        <input type="submit" value="+">
+                        <input type="submit" name="submit" value="+">
                     </div>
 
+
+                    <?php
                     
+                        if (isset($_POST['submit'])){
+
+                            $item_name = $_POST['item_name'];
+                            $item_stock = $_POST['item_stock'];
+                            $item_price = $_POST['item_price'];
+                            $item_info = $_POST['item_info'];
+                            $item_seller = $_SESSION['business_id'];
+
+                            $insert = "INSERT INTO inventory (business_id, item_name, item_description, category, price, stock) VALUES ($item_seller, '$item_name', '$item_info', 'ECOMMERCE', $item_price, $item_stock)";
+                            $query = mysqli_query($conn, $insert);
+
+                            $upload_count = count($_FILES['item_group']['name']);
+                            $dir = "Items";
+
+                            $command = "SELECT * FROM inventory WHERE item_name = '$item_name' AND business_id = '$item_seller'";
+                            $read = mysqli_query($conn, $command);
+                            $qValue = mysqli_fetch_array($read);
+                            $business_id = $qValue['product_id'];
+                            for($i = 0; $i < $upload_count; $i++){
+                                $filename = $_FILES['item_group']['name'][$i];
+                                $upload = move_uploaded_file($_FILES['item_group']['tmp_name'][$i], $dir. "/". $_FILES['item_group']['name'][$i]);
+                                $insert = "INSERT INTO product_photos (product_id, filename) values ($business_id, '$filename')";
+                                $query = mysqli_query($conn, $insert);
+                            }
+
+                            header("Location: manage-online-store.php");
+
+                        }
+                    
+                    
+                    ?>
+       
 
                 </div>
             </form>
             <!-- Displaying Items -->
 
-
-
-
-            
             <?php 
 
                 if (mysqli_num_rows($result) == 0){
@@ -139,12 +170,36 @@ $result = mysqli_query($conn, $query);
             ?>
             
 
-            <form action="add-business.php" method="post" id="delete-form">
+            <form action="manage-online-store.php" method="POST" id="delete-form">
                     <div class="delete-form-container">
-                        <input type="text" name="business" id="delete-business" placeholder="Enter Account Name">
+                        <input type="text" name="product_id" id="delete-business" placeholder="Enter Product ID">
                         <input type="submit" name="Delete" value="Delete" id="Delete">
                     </div>       
             </form>
+
+
+            <?php 
+                    
+                    if(isset($_POST['Delete'])){
+                        $del_name = $_POST['product_id'];
+
+                        $query = "DELETE FROM product_photos WHERE product_id = $del_name";
+                        mysqli_query($conn, $query);
+                        
+                        $query = "DELETE FROM inventory WHERE product_id = $del_name";
+
+                        if (!mysqli_query($conn, $query)){
+                            echo("Error description: " . mysqli_error($conn));
+                            echo "<h3 style = 'color:red; text-align:center'>No Existing Account Name</h3>";
+                        } else {
+                            echo "<h3> The Change will apply after the refresh</h3>";
+                        }
+
+                    }
+                
+                
+            ?>
+
         </div>
         <div class="right"></div>
     </section>
